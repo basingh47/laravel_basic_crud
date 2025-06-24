480ce06d92640d9d34e99c3e3a3d758e3720b6b9
ankitsingh-iq <ankit.singh@iqinfinite.in>
2025-06-24 17:40:35 +0530

    add Page login/Registration

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard.index');
 });
+
+Route::get('/create-post', function () {
+    return view('admin.pages.create-post');
+})->name('create-post');
+
+
+Route::get('/list-posts', function () {
+    return view('admin.pages.list-posts');
+})->name('list-posts');
+
+Route::get('/tags', function () {
+    return view('admin.pages.tags');
+})->name('tags');
+
+Route::get('/settings', function () {
+    return view('admin.pages.settings');
+})->name('settings');
+
+
+Route::get('/registration', function () {
+    return view('admin.auth.registration');
+});
+
+Route::resource('categories',CategoryController::class);
